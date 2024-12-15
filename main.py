39c60d5604a772f56d2f7f05d8ad4351821dc8b5
Karan Parashar <karan@pixelos.net>
2024-12-15 14:35:07 +0530

    Redirect to github repo on /

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from routers import slcm
 
 app = FastAPI()
+
+
+@app.get("/")
+async def redirect():
+    return RedirectResponse("https://github.com/whyredfire/betterslcm")
 
 
 @app.get("/status")
