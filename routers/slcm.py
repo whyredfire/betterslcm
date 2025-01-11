f871d742928e446c76d281b5e533191c6fd394a2
Karan Parashar <karan@pixelos.net>
2025-01-12 04:08:21 +0530

    "Overhaul"

--- routers/slcm.py
+++ routers/slcm.py
@@ -5,18 +5,32 @@ import requests
 
 from schemas import Credentials
 
-
-LOGIN_URL = "https://mujslcm.jaipur.manipal.edu"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0",
 }
+API_REF = {
+    "login": "https://mujslcm.jaipur.manipal.edu",
+    "logout": "/Home/Logout",
+    "attendance": "/Student/Academic/GetAttendanceSummaryList",
+    "cgpa": "/Student/Academic/GetCGPAGPAForFaculty",
+    "grades": "/Student/Academic/GetGradesForFaculty",
+    "internal_marks": "/Student/Academic/GetInternalMarkForFaculty",
+}
 
 
-def format_cookies(request):
-    return {
-        "__RequestVerificationToken": request.cookies.get("verification_token"),
-        "ASP.NET_SessionId": request.cookies.get("session_id"),
-    }
+def make_request(endpoint, request, data=None):
+    try:
+        return requests.post(
+            url=API_REF["login"] + API_REF[endpoint],
+            data=data,
+            headers=HEADERS,
+            cookies={
+                "__RequestVerificationToken": request.cookies.get("verification_token"),
+                "ASP.NET_SessionId": request.cookies.get("session_id"),
+            },
+        ).json()
+    except requests.exceptions.JSONDecodeError:
+        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
 
 
 app = APIRouter()
@@ -25,7 +39,9 @@ app = APIRouter()
 @app.post("/login")
 async def login(creds: Credentials, response: Response):
 
-    _res = requests.get(LOGIN_URL, headers=HEADERS, allow_redirects=False, timeout=5)
+    _res = requests.get(
+        API_REF["login"], headers=HEADERS, allow_redirects=False, timeout=5
+    )
     soup = BeautifulSoup(_res.content, "html.parser")
 
     _token = soup.find("input", {"name": "__RequestVerificationToken"})["value"]
@@ -42,7 +58,7 @@ async def login(creds: Credentials, response: Response):
     }
 
     res = requests.post(
-        url=LOGIN_URL,
+        url=API_REF["login"],
         data=payload,
         headers=HEADERS,
         cookies=cookies,
@@ -54,7 +70,7 @@ async def login(creds: Credentials, response: Response):
 
     session_id = res.headers.get("Set-Cookie").split(";")[0].split("=")[1]
 
-    response.set_cookie(key="verification_token", value=cookie)
+    response.set_cookie(key="verification_token", value=cookie, expires="Session")
     response.set_cookie(key="session_id", value=session_id)
     return {"message": "logged in"}
 
@@ -62,83 +78,56 @@ async def login(creds: Credentials, response: Response):
 @app.get("/logout")
 async def logout(request: Request, response: Response):
 
-    if not (
-        request.cookies.get("verification_token") or request.cookies.get("session_id")
-    ):
-        raise HTTPException(status_code=200, detail={"message": "user not logged in"})
-
     requests.post(
-        url=f"{LOGIN_URL}/Home/Logout", headers=HEADERS, cookies=format_cookies(request)
+        url=API_REF["login"] + API_REF["logout"],
+        headers=HEADERS,
+        cookies=request.cookies.get("session_id"),
     )
-
-    response.delete_cookie("verification_token")
-    response.delete_cookie("session_id")
 
     return {"message": "user logged out"}
 
 
 @app.get("/attendance")
 async def attendance(request: Request):
-    response = requests.post(
-        url=f"{LOGIN_URL}/Student/Academic/GetAttendanceSummaryList",
-        data={"StudentCode": ""},
-        headers=HEADERS,
-        cookies=format_cookies(request),
-    )
-
-    try:
-        return response.json()["AttendanceSummaryList"]
-    except requests.exceptions.JSONDecodeError:
-        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
+    return make_request("attendance", request=request, data={"StudentCode": ""})[
+        "AttendanceSummaryList"
+    ]
 
 
 @app.get("/cgpa")
 async def cgpa(request: Request):
-    response = requests.post(
-        url=f"{LOGIN_URL}/Student/Academic/GetCGPAGPAForFaculty",
-        data={"Enrollment": "", "AcademicYear": "", "ProgramCode": ""},
-        headers=HEADERS,
-        cookies=format_cookies(request),
-    )
-
-    try:
-        return {
-            i: j
-            for i, j in response.json()["InternalMarksList"][0].items()
-            if j not in [None, "null", "-"]
-            and i not in ["sno", "RegistrationNo", "StudentName"]
-        }
-    except requests.exceptions.JSONDecodeError:
-        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
+    return {
+        i: j
+        for i, j in make_request(
+            "cgpa",
+            request=request,
+            data={"Enrollment": "", "AcademicYear": "", "ProgramCode": ""},
+        )["InternalMarksList"][0].items()
+        if j not in [None, "null", "-"]
+        and i not in ["sno", "RegistrationNo", "StudentName"]
+    }
 
 
 @app.get("/grades")
 async def grades(request: Request, semester: int):
-    print(request.cookies.get("verification_token"))
-
-    response = requests.post(
-        url=f"{LOGIN_URL}/Student/Academic/GetGradesForFaculty",
-        data={"Enrollment": "", "Semester": toRoman(semester)},
-        headers=HEADERS,
-        cookies=format_cookies(request),
+    return (
+        {
+            i: j
+            for i, j in subject.items()
+            if j not in [None, "null", "-"] and i != "sno"
+        }
+        for subject in make_request(
+            "grades",
+            request=request,
+            data={"Enrollment": "", "Semester": toRoman(semester)},
+        )["InternalMarksList"]
     )
-
-    try:
-        return response.json()["InternalMarksList"]
-    except requests.exceptions.JSONDecodeError:
-        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
 
 
 @app.get("/internal_marks")
 async def internal_marks(request: Request, semester: int):
-    response = requests.post(
-        url=f"{LOGIN_URL}/Student/Academic/GetInternalMarkForFaculty",
+    return make_request(
+        "internal_marks",
+        request=request,
         data={"Enrollment": "", "Semester": toRoman(semester)},
-        headers=HEADERS,
-        cookies=format_cookies(request),
-    )
-
-    try:
-        return response.json()["InternalMarksList"]
-    except requests.exceptions.JSONDecodeError:
-        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
+    )["InternalMarksList"]
