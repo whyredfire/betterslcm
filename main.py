861ec0711304093484cd2d52b53d2cd9da679d7e
Karan Parashar <karan@pixelos.net>
2025-01-12 00:46:41 +0530

    Improve code logic and handle errors
    - Merge internal/betterslcm.py with routers
    - Handle Internal server errors properly

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from fastapi.responses import RedirectResponse
 
 from routers import slcm
 
 app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+async def validation_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code, content=jsonable_encoder(exc.detail)
+    )
 
 
 @app.get("/")
--- routers/slcm.py
+++ routers/slcm.py
@@ -1,59 +1,144 @@
-from fastapi import APIRouter, Response, Request
+from bs4 import BeautifulSoup
+from fastapi import APIRouter, Response, Request, HTTPException
+from roman import toRoman
+import requests
 
-from internal import betterslcm
 from schemas import Credentials
+
+
+LOGIN_URL = "https://mujslcm.jaipur.manipal.edu"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0",
+}
+
+
+def format_cookies(request):
+    return {
+        "__RequestVerificationToken": request.cookies.get("verification_token"),
+        "ASP.NET_SessionId": request.cookies.get("session_id"),
+    }
+
 
 app = APIRouter()
 
 
 @app.post("/login")
 async def login(creds: Credentials, response: Response):
-    res = betterslcm.login(creds.username, creds.password)
-    if not res:
-        return False
 
-    cookie, asp_net_cookie = res[0], res[1]
+    _res = requests.get(LOGIN_URL, headers=HEADERS, allow_redirects=False, timeout=5)
+    soup = BeautifulSoup(_res.content, "html.parser")
 
-    response.set_cookie(key="cookie", value=cookie)
-    response.set_cookie(key="asp_net_cookie", value=asp_net_cookie)
+    _token = soup.find("input", {"name": "__RequestVerificationToken"})["value"]
+    _cookies = _res.headers.get("Set-Cookie").split(";")[0]
+    cookie = _cookies.split("=")[1]
+
+    cookies = {"__RequestVerificationToken": cookie}
+    payload = {
+        "__RequestVerificationToken": _token,
+        "EmailFor": "@muj.manipal.edu",
+        "LoginFor": "2",
+        "UserName": creds.username,
+        "Password": creds.password,
+    }
+
+    res = requests.post(
+        url=LOGIN_URL,
+        data=payload,
+        headers=HEADERS,
+        cookies=cookies,
+        allow_redirects=False,
+    )
+
+    if not res.headers.get("Set-Cookie"):
+        raise HTTPException(status_code=401, detail={"message": "invalid credentials"})
+
+    session_id = res.headers.get("Set-Cookie").split(";")[0].split("=")[1]
+
+    response.set_cookie(key="verification_token", value=cookie)
+    response.set_cookie(key="session_id", value=session_id)
     return {"message": "logged in"}
 
 
 @app.get("/logout")
-async def logout(request: Request):
-    cookie = request.cookies.get("cookie")
-    asp_net_cookie = request.cookies.get("asp_net_cookie")
+async def logout(request: Request, response: Response):
 
-    return betterslcm.logout(cookie, asp_net_cookie)
+    if not (
+        request.cookies.get("verification_token") or request.cookies.get("session_id")
+    ):
+        raise HTTPException(status_code=200, detail={"message": "user not logged in"})
+
+    requests.post(
+        url=f"{LOGIN_URL}/Home/Logout", headers=HEADERS, cookies=format_cookies(request)
+    )
+
+    response.delete_cookie("verification_token")
+    response.delete_cookie("session_id")
+
+    return {"message": "user logged out"}
 
 
 @app.get("/attendance")
 async def attendance(request: Request):
-    cookie = request.cookies.get("cookie")
-    asp_net_cookie = request.cookies.get("asp_net_cookie")
+    response = requests.post(
+        url=f"{LOGIN_URL}/Student/Academic/GetAttendanceSummaryList",
+        data={"StudentCode": ""},
+        headers=HEADERS,
+        cookies=format_cookies(request),
+    )
 
-    return betterslcm.get_attendance(cookie, asp_net_cookie)
+    try:
+        return response.json()["AttendanceSummaryList"]
+    except requests.exceptions.JSONDecodeError:
+        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
 
 
 @app.get("/cgpa")
 async def cgpa(request: Request):
-    cookie = request.cookies.get("cookie")
-    asp_net_cookie = request.cookies.get("asp_net_cookie")
+    response = requests.post(
+        url=f"{LOGIN_URL}/Student/Academic/GetCGPAGPAForFaculty",
+        data={"Enrollment": "", "AcademicYear": "", "ProgramCode": ""},
+        headers=HEADERS,
+        cookies=format_cookies(request),
+    )
 
-    return betterslcm.get_cgpa(cookie, asp_net_cookie)
+    try:
+        return {
+            i: j
+            for i, j in response.json()["InternalMarksList"][0].items()
+            if j not in [None, "null", "-"]
+            and i not in ["sno", "RegistrationNo", "StudentName"]
+        }
+    except requests.exceptions.JSONDecodeError:
+        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
 
 
 @app.get("/grades")
 async def grades(request: Request, semester: int):
-    cookie = request.cookies.get("cookie")
-    asp_net_cookie = request.cookies.get("asp_net_cookie")
+    print(request.cookies.get("verification_token"))
 
-    return betterslcm.get_grades(cookie, asp_net_cookie, semester)
+    response = requests.post(
+        url=f"{LOGIN_URL}/Student/Academic/GetGradesForFaculty",
+        data={"Enrollment": "", "Semester": toRoman(semester)},
+        headers=HEADERS,
+        cookies=format_cookies(request),
+    )
+
+    try:
+        return response.json()["InternalMarksList"]
+    except requests.exceptions.JSONDecodeError:
+        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
 
 
 @app.get("/internal_marks")
 async def internal_marks(request: Request, semester: int):
-    cookie = request.cookies.get("cookie")
-    asp_net_cookie = request.cookies.get("asp_net_cookie")
+    response = requests.post(
+        url=f"{LOGIN_URL}/Student/Academic/GetInternalMarkForFaculty",
+        data={"Enrollment": "", "Semester": toRoman(semester)},
+        headers=HEADERS,
+        cookies=format_cookies(request),
+    )
 
-    return betterslcm.get_grades(cookie, asp_net_cookie, semester)
+    try:
+        return response.json()["InternalMarksList"]
+    except requests.exceptions.JSONDecodeError:
+        raise HTTPException(status_code=401, detail={"message": "unauthorized"})
